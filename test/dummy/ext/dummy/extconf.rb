3ffc8af2be11cdf527859d76b17f65fa67fa68fc
Peter Wagenet <peter.wagenet@gmail.com>
2018-09-12 17:26:25 -0700

    Try without special Windows behavior

--- test/dummy/ext/dummy/extconf.rb
+++ test/dummy/ext/dummy/extconf.rb
@@ -4,9 +4,9 @@ root_dir = File.expand_path("../../../../../..", __FILE__)
 
 dir_config "dummy"
 
-if RUBY_PLATFORM =~ /mingw/
-  raise "not implemented"
-  append_ldflags("-L#{root_dir}/windows_build -lhelix-runtime-#{HelixRuntime::VERSION.gsub('.', '-')}")
-end
+# if RUBY_PLATFORM =~ /mingw/
+#   raise "not implemented"
+#   append_ldflags("-L#{root_dir}/windows_build -lhelix-runtime-#{HelixRuntime::VERSION.gsub('.', '-')}")
+# end
 
 create_makefile "dummy"
